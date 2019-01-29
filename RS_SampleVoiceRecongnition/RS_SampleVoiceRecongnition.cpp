fafb198b093e5f1fcd00523fa1cb97dd5af4d914
Izayoi065 <necrofantasia.voyage1970@gmail.com>
2019-01-29 16:32:07 +0900

    プロジェクト ファイルを追加します。

---
+++ RS_SampleVoiceRecongnition/RS_SampleVoiceRecongnition.cpp
@@ -0,0 +1,74 @@
+﻿// RS_SampleVoiceRecongnition.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include "pch.h"
+
+int main()
+{
+	std::locale::global(std::locale("japanese"));
+
+	PXCSession *session = PXCSession::CreateInstance();
+	if (session == nullptr) {
+		wprintf_s(L"Session not created by PXCSession\n");
+		return 1;
+	}
+
+	PXCSpeechRecognition *sr = nullptr;
+	pxcStatus sts = session->CreateImpl<PXCSpeechRecognition>(&sr);
+	if (sts != pxcStatus::PXC_STATUS_NO_ERROR) {
+		wprintf_s(L"Failed to create an instance of the PXCSpeechRecognition\n");
+		return 2;
+	}
+
+	PXCSpeechRecognition::ProfileInfo pinfo;
+	sr->QueryProfile(0, &pinfo);
+
+	pinfo.language = PXCSpeechRecognition::LanguageType::LANGUAGE_JP_JAPANESE;
+
+	sts = sr->SetProfile(&pinfo);
+
+	if (sts != pxcStatus::PXC_STATUS_NO_ERROR) {
+		wprintf_s(L"Failed to Configure the Module\n");
+		return 3;
+	}
+
+	sts = sr->SetDictation();
+	if (sts != pxcStatus::PXC_STATUS_NO_ERROR) {
+		wprintf_s(L"Failed to set the recognition mode\n");
+		return 4;
+	}
+
+	class MyHandler : public PXCSpeechRecognition::Handler {
+	public:
+		virtual void PXCAPI OnRecognition(const PXCSpeechRecognition::RecognitionData *data) {
+			wprintf_s(L"Output %s\n", data->scores[0].sentence);
+		}
+
+		virtual void PXCAPI OnAlert(const PXCSpeechRecognition::AlertData *data) {
+			if (data->label == PXCSpeechRecognition::ALERT_SPEECH_BEGIN) {
+				wprintf_s(L"Alert: SPEECH_BEGIN\n");
+			}
+			else if (data->label == PXCSpeechRecognition::ALERT_SPEECH_END) {
+				wprintf_s(L"Alert: SPEECH_END\n");
+			}
+			else if (data->label == PXCSpeechRecognition::ALERT_VOLUME_LOW) {
+				wprintf_s(L"Alert: VOLUME_LOW\n");
+			}
+		}
+	};
+
+	MyHandler handler;
+
+	sts = sr->StartRec(nullptr, &handler);
+	if (sts != pxcStatus::PXC_STATUS_NO_ERROR) {
+		wprintf_s(L"Failed to start the handler\n");
+		return 5;
+	}
+
+	while (true) {
+		if (GetAsyncKeyState(VK_ESCAPE)) break;
+	}
+
+	sr->StopRec();
+	return 0;
+}
---
+++ RS_SampleVoiceRecongnition/pch.h
@@ -0,0 +1,28 @@
+﻿// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
+/* 追加ライブラリへのリンク */
+#ifdef _DEBUG
+#define RSSDK_EXT "_d.lib"
+#else
+#define RSSDK_EXT ".lib"
+#endif
+#pragma comment( lib, "libpxc" RSSDK_EXT)
+#pragma comment( lib, "libpxcutils" RSSDK_EXT)
+
+#ifndef PCH_H
+#define PCH_H
+
+// TODO: ここでプリコンパイルするヘッダーを追加します
+#include <iostream>
+#include <Windows.h>
+
+/* Real Sense SDK 関連 */
+#include "pxcsensemanager.h"
+#include "pxcspeechrecognition.h"
+
+#endif //PCH_H
